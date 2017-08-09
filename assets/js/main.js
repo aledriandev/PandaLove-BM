ab9c1d2e85313c94fe7ad71ee3fbe1e817a5e35d
AQP20171679 <alejandraadriantejada@gmail.com>
2017-08-09 04:05:43 -0500

    Adicionando imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+var contenedor = document.getElementById('contenedor-imagenes-panda');
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+for (var i = 0; i < pictures.length; i++) {
+	var picture = document.createElement("img");
+	picture.src = pictures[i];
+	picture.setAttribute('class', 'picture-panda');
+	contenedor.appendChild(picture);
+}
